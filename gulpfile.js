45f93e705622c3376dc3576718b3a3fafb12bb15
Peter Ilyash <p.ilyash.j@gmail.com>
2020-09-09 14:54:08 +0300

    Create project structure

---
+++ gulpfile.js
@@ -0,0 +1,147 @@
+const { src, dest, parallel, series, watch } = require('gulp');
+
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+
+const pug = require('gulp-pug');
+
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const modifyCssUrls = require('gulp-modify-css-urls');
+
+const imageMin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+
+const del = require('del');
+
+//BROWSER 'SYNC'
+
+function initBrowserSync() {
+	browserSync.init({
+		server: { baseDir: 'app/html/' },
+		notify: false,
+		open: false
+	});
+}
+
+// MARKUP
+
+function compilateToHTML() {
+	return src('app/pug/**/*.pug')
+		.pipe(pug())
+		.pipe(dest('app/html/'))
+		.pipe(browserSync.stream());
+}
+
+// STYLES
+
+const styles = series(concatSass, compilateToCSS, modifyUrls);
+
+let sassModules = [
+	'app/sass/config.sass',
+	'app/sass/common/*.sass',
+	'app/sass/elements/*.sass',
+	'app/sass/structure-sections/**/*.sass'];
+
+function concatSass() {
+	return src(sassModules)
+		.pipe(concat('styles.min.sass'))
+		.pipe(dest('app/sass/'));
+}
+
+function compilateToCSS() {
+	return src('app/sass/styles.min.sass')
+		.pipe(sass())
+		.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+		.pipe(cleanCSS(({ level: { 1: { specialComments: 0 } }/*, format: 'beautify'*/ })))
+		.pipe(dest('app/css/'))
+		.pipe(browserSync.stream());
+}
+
+function modifyUrls() {
+	return src('app/css/styles.min.css')
+		.pipe(modifyCssUrls({
+			modify: (url, filePath) => {
+				let newUrl = url;
+				let reverseCwd = '../';
+
+				if (url.includes('/img/')) newUrl = url.slice(reverseCwd.length);
+
+				return newUrl;
+			},
+
+			prepend: '',
+			append: ''
+		}))
+		.pipe(dest('app/css/'));
+}
+
+// SCRIPTS
+
+let jsModules = ['app/js/custom/*.js'];
+
+function buildScripts() {
+	return src(jsModules)
+		.pipe(concat('app.min.js'))
+		.pipe(uglify())
+		.pipe(dest('app/js/'))
+		.pipe(browserSync.stream());
+}
+
+//IMAGES
+
+function minimazeImages() {
+	return src('app/img/**/*')
+		.pipe(newer('dist/img/*'))
+		.pipe(imageMin())
+		.pipe(dest('dist/img/'));
+}
+
+function cleanMinImages() {
+	return del('dist/img/**/*', { force: true });
+}
+
+// BUILD
+
+function buildCopy() {
+	return src([
+		'app/css/*.min.css',
+		'app/js/*.min.js',
+		'app/img/dest/*',
+		'app/*.html',
+		'app/fonts/**/*'
+	], { base: 'app' })
+		.pipe(dest('dist'));
+}
+
+function cleanDist() {
+	return del('dist/**/*', { force: true });
+}
+
+// WATCH
+
+function startWatching() {
+	watch('app/pug/*.pug', compilateToHTML);
+	watch(['app/sass/**/*.sass', '!app/sass/styles.min.sass', '!app/sass/temp/*.sass'], styles);
+	watch(['app/js/*.js', '!app/js/app.min.js'], buildScripts);
+	watch('app/img/*', minimazeImages);
+}
+
+
+exports.initBrowserSync = initBrowserSync;
+
+exports.markup = compilateToHTML;
+exports.styles = styles;
+exports.scripts = buildScripts;
+
+const transformCode = series(compilateToHTML, styles, buildScripts);
+
+exports.minimazeImages = minimazeImages;
+exports.cleanMinImages = cleanMinImages;
+
+exports.build = series(cleanDist, transformCode, buildCopy, minimazeImages);
+
+exports.default = parallel(transformCode,
+	series(initBrowserSync, browserSync.reload), startWatching);
