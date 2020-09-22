7aa912e82315a97c21e969371f2f3f1dbbe9fff9
Peter Ilyash <p.ilyash.j@gmail.com>
2020-09-22 13:54:33 +0300

    Fix problem with double click on the buttons
    Earlier a double click is followed by slider crash.
    Now 'displayNewItem' function is rewritten in 'async/await' style and
    works correctly.

--- app/js/custom/actors-slider.js
+++ app/js/custom/actors-slider.js
@@ -1,47 +1,52 @@
 $(function () {
   let items = [];
-
-  wrapImgAndInfo();
-  createItems();
-
-  console.log(items[0]);
-
-  $(items[0]).addClass('active');
-
-  for (let i = 1; i < items.length; i++) {
-    $(items[i]).hide();
-  }
-
   let duration = 500;
+  let isBlock = false;
+
+  inizializeSlider();
 
   $('#actors-slider .nav-btn-prev').on('click', () => displayNewElem('prev'));
   $('#actors-slider .nav-btn-next').on('click', () => displayNewElem('next'));
 
-  function wrapImgAndInfo() {
-    $('#actors-slider .s-actors-image-container img').wrap('<div class="as-image-wrapper"></div>');
-    $('#actors-slider .s-actors-information').wrap('<div class="as-info-wrapper"></div>');
-    $('#actors-slider .as-info-wrapper').wrapAll('<div class="as-info"></div>');
-  }
+  function inizializeSlider() {
+    wrapImgAndInfo();
+    createItems();
 
-  function createItems() {
-    for (let i = 0; i < $('#actors-slider .as-image-wrapper').length; i++) {
-      let item = [
-        $('#actors-slider .as-image-wrapper')[i],
-        $('#actors-slider .as-info-wrapper')[i]
-      ];
+    $(items[0]).addClass('active');
 
-      items.push(item);
+    for (let i = 1; i < items.length; i++) {
+      $(items[i]).hide();
+    }
+
+    function wrapImgAndInfo() {
+      $('#actors-slider .s-actors-portrait').wrap('<div class="as-image-wrapper"></div>');
+      $('#actors-slider .s-actors-information').wrap('<div class="as-info-wrapper"></div>');
+      $('#actors-slider .as-info-wrapper').wrapAll('<div class="as-info"></div>');
+    }
+
+    function createItems() {
+      for (let i = 0; i < $('#actors-slider .as-image-wrapper').length; i++) {
+        let item = [
+          $('#actors-slider .as-image-wrapper')[i],
+          $('#actors-slider .as-info-wrapper')[i]
+        ];
+
+        items.push(item);
+      }
     }
   }
 
-  function displayNewElem(direction) {
+  async function displayNewElem(direction) {
+    if (isBlock) return;
+    isBlock = true;
+
     let currentItem = items.find((item) => $(item).hasClass('active'));
     let newItem = selectNewItem(direction);
 
-    $(currentItem).removeClass('active');
-    $(currentItem).fadeOut(duration);
+    await $(currentItem).removeClass('active').fadeOut(duration).promise();
+    await $(newItem).addClass('active').fadeIn(duration).promise();
 
-    setTimeout(() => $(newItem).addClass('active').fadeIn(duration), duration);
+    isBlock = false;
 
     function selectNewItem(direction) {
       let newItem;
@@ -52,7 +57,6 @@ $(function () {
       if (!newItem.length && direction === 'prev') newItem = items[items.length - 1];
       if (!newItem.length && direction === 'next') newItem = items[0];
 
-      console.log(newItem);
       return newItem;
     }
   }
