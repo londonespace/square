25336d432a64f44496afea54bf89dde743e7cae6
Peter Ilyash <p.ilyash.j@gmail.com>
2020-09-22 12:37:39 +0300

    Add photo transition effect at the click
    Then click on the navigation buttons changes the photos.

--- app/js/custom/actors-slider.js
+++ app/js/custom/actors-slider.js
@@ -1,9 +1,26 @@
-let items = [];
+$(function () {
+  let items = [];
 
-function inizializeActorsSlider() {
+  wrapImgAndInfo();
+  createItems();
+
+  console.log(items[0]);
+
+  $(items[0]).addClass('active');
+
+  for (let i = 1; i < items.length; i++) {
+    $(items[i]).hide();
+  }
+
+  let duration = 500;
+
+  $('#actors-slider .nav-btn-prev').on('click', () => displayNewElem('prev'));
+  $('#actors-slider .nav-btn-next').on('click', () => displayNewElem('next'));
+
   function wrapImgAndInfo() {
     $('#actors-slider .s-actors-image-container img').wrap('<div class="as-image-wrapper"></div>');
     $('#actors-slider .s-actors-information').wrap('<div class="as-info-wrapper"></div>');
+    $('#actors-slider .as-info-wrapper').wrapAll('<div class="as-info"></div>');
   }
 
   function createItems() {
@@ -17,30 +34,26 @@ function inizializeActorsSlider() {
     }
   }
 
-  wrapImgAndInfo();
-  createItems();
+  function displayNewElem(direction) {
+    let currentItem = items.find((item) => $(item).hasClass('active'));
+    let newItem = selectNewItem(direction);
 
-  $(items[0]).addClass('active');
+    $(currentItem).removeClass('active');
+    $(currentItem).fadeOut(duration);
 
-  for (let i = 1; i < items.length; i++) {
-    $(items[i]).hide();
+    setTimeout(() => $(newItem).addClass('active').fadeIn(duration), duration);
+
+    function selectNewItem(direction) {
+      let newItem;
+
+      if (direction === 'prev') newItem = $(currentItem).prev();
+      if (direction === 'next') newItem = $(currentItem).next();
+
+      if (!newItem.length && direction === 'prev') newItem = items[items.length - 1];
+      if (!newItem.length && direction === 'next') newItem = items[0];
+
+      console.log(newItem);
+      return newItem;
+    }
   }
-}
-
-inizializeActorsSlider();
-
-// $(function () {
-//   let duration = 500;
-
-//   $('#actors-slider .nav-prev').on('click', displayPrevElem);
-
-//   function displayPrevElem() {
-//     let currentItem = $('#actors-slider .as-item-wrapper.active');
-//     let prevItem = currentItem.next();
-
-//     currentItem.removeClass('active').css('display: block');
-//     currentItem.fadeOut(duration);
-
-//     setTimeout(() => prevItem.addClass('active').fadeIn(duration), duration);
-//   }
-// });
+});
