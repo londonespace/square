83bf9dd36a685f0da8c52cda0a726f5ef87fc28d
Peter Ilyash <p.ilyash.j@gmail.com>
2020-09-23 14:42:19 +0300

    Change the navigation panel hide effect
    It is changed from minimizing to sliding. This entailes significant
    changes in the 'actors-nav.js' code.

--- app/js/custom/actors-nav.js
+++ app/js/custom/actors-nav.js
@@ -12,7 +12,6 @@ $(function () {
     }
   }
 
-  $(button).addClass('in-nav');
   $(button).on('click', onClick);
 
   function onClick() {
@@ -23,33 +22,12 @@ $(function () {
     }
   }
 
-  async function minimizeNav() {
-    await $(button).appendTo('#actors-slider')
-      .css({ 'left': cord.x, 'bottom': cord.y })
-      .toggleClass('in-nav out-nav').promise();
-
-    $(button).css({ 'left': 0, 'bottom': 0 })
-      .on('transitionend', onTransitionEnd);
-
-    $(container).addClass('minimized');
-
-    function onTransitionEnd() {
-      $(button).css({ 'left': '', 'bottom': '' })
-        .off('transitionend', onTransitionEnd);
-    }
+  function minimizeNav() {
+    $(container).css('transform', `translate(-${cord.x}px, ${cord.y}px)`)
+      .addClass('minimized');
   }
 
   function maximizeNav() {
-    $(button).css({ 'left': cord.x, 'bottom': cord.y })
-      .on('transitionend', onTransitionEnd);
-
-    $(container).removeClass('minimized');
-
-    function onTransitionEnd() {
-      $(button).appendTo(container)
-        .toggleClass('in-nav out-nav')
-        .css({ 'left': '', 'bottom': '' })
-        .off('transitionend', onTransitionEnd);
-    }
+    $(container).css('transform', 'translate(0)').removeClass('minimized');
   }
 })
