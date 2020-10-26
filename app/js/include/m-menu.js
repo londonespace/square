b02649877f6aff601763d5105547148e0fc2b9b3
Peter Ilyash <p.ilyash.j@gmail.com>
2020-10-26 08:51:16 +0300

    Update header hamburger and menu
    The effect of hamburger animation is added and some design revisions
    are made.
    
    Also gitignore is updated.

--- app/js/include/m-menu.js
+++ app/js/include/m-menu.js
@@ -1,13 +1,38 @@
 document.addEventListener(
   'DOMContentLoaded', () => {
-    new Mmenu("#m-menu", {
+    let menu = new Mmenu("#m-menu", {
       extensions: ['pagedim-black'],
 
       navbar: {
         title: `<div class="m-menu-logo-wrapper">
           <img src="../img/square-logo-dark.svg" 
-          class="m-menu-square-logo-dark" alt="Салон красоты Митлер"></div>`
+          class="m-menu-square-logo-dark" alt="Square"></div>`
       }
     });
+
+    let menuParentElem = document.querySelector('#m-menu');
+    let hamburger = document.querySelector('.hamburger');
+
+    let observer = new MutationObserver(toggleHamburger);
+
+    observer.observe(document.querySelector('#m-menu'),
+      { attributes: true, attributeFilter: ['class'] });
+
+    function toggleHamburger() {
+      console.log(1);
+      let isHambActive = hamburger.classList.contains('is-active');
+      let isMenuActive = menuParentElem.classList.contains('mm-menu_opened');
+
+      if (!isMenuActive && isHambActive) {
+        hamburger.classList.remove('is-active');
+      }
+
+      if (isMenuActive && !isHambActive) {
+        setTimeout(
+          () => hamburger.classList.add('is-active'),
+          menu.conf.transitionDuration
+        );
+      }
+    }
   }
 );
